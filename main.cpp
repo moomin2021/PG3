ddf1e730ffa50b648331307b9d56dbe45caa0e73
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-10-03 11:00:45 +0900

    1-2確認課題

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// --一般的な賃金体系-- //
+int Case1(int value, int hour) {
+	return value * hour;
+}
+
+// --再帰的な賃金体系-- //
+int Case2(int value, int hour) {
+	// --時間が0になったら-- //
+	if (hour <= 0) {
+		return 0;
+	}
+
+	return value + Case2(value * 2 - 50, hour - 1);
+}
+
 int main() {
 
+	printf("一般的な賃金%d\n", Case1(1072, 8));
+	printf("再帰的な賃金%d\n", Case2(100, 8));
 	return 0;
 }
