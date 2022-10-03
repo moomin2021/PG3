253b416bf7b3f06988f2f1211e74c2385b6053a1
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-10-03 10:13:53 +0900

    1-1の確認課題

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+template <typename Type>
 
+Type Min(Type a, Type b) {
+	if (a < b) return a;
+	return b;
+}
+
+template <>
+char Min(char a, char b) {
+	printf("”š‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ\n");
+	return 0;
+}
+
+int main() {
+	printf("%d\n", Min<int>(10, 5));
+	printf("%f\n", Min<float>(7.5f, 5.5f));
+	printf("%lf\n", Min<double>(11.1, 2.3));
+	printf("%c\n", Min<char>('a', 'b'));
 	return 0;
 }
