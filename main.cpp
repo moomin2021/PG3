a09b2027abf2796a061cc3bae75a393716ff95f3
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-10-17 10:36:46 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,33 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <time.h>
 
-// --一般的な賃金体系-- //
-int GeneralWage(int value, int hour) {
-	return value * hour;
+typedef void (*PFunc)(int*);
+
+void CallBack(int* s) {
+	printf_s("%d秒間処理が止まります\n", *s);
 }
 
-// --再帰的な賃金体系-- //
-int RecursiveWage(int value, int hour) {
-	// --時間が0になったら-- //
-	if (hour <= 0) {
-		return 0;
-	}
-
-	return value + RecursiveWage(value * 2 - 50, hour - 1);
+void SetTimeOut(PFunc p, int second) {
+	p(&second);
+	Sleep(second * 1000);
 }
 
 int main() {
+	// --初期化-- //
+	srand(time(nullptr));
 
-	printf("一般的な賃金%d\n", GeneralWage(1072, 8));
-	printf("再帰的な賃金%d\n", RecursiveWage(100, 8));
+	char str[5];
+	while (true) {
+		// --入力データ取得-- //
+		printf_s("[奇数]か[偶数]か入力してください\n");
+		scanf_s("%s", str, 5);
 
-	if (GeneralWage(1072, 8) > RecursiveWage(100, 8)) {
-		printf("一般的な賃金の方が高い\n");
+		// --入力したのが[偶数]か[奇数]ならループを抜ける-- //
+		if (strcmp(str, "偶数") == 0) break;
+		if (strcmp(str, "奇数") == 0) break;
+
+		// --入力したのが[偶数]か[奇数]でなかった場合-- //
+		printf_s("間違った入力です。\n");
 	}
 
-	else {
-		printf("再帰的な賃金の方が高い\n");
-	}
+	// --関数ポインタを用いて一時的に処理を止める-- //
+	PFunc p;
+	p = CallBack;
+	SetTimeOut(p, 3);
+
+	// --ランダムな値を取得-- //
+	int randomValue = rand();
+
+	printf_s("ランダムで返された値は%dです\n", randomValue);
 
 	return 0;
 }
