f54e6d3d49f75d3b73bda4fa7d701e22dbcbad94
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-12-02 14:38:02 +0900

    あ

--- main.cpp
+++ main.cpp
@@ -1,7 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct cell{
+	int val;
+	struct cell* next;
+} CELL;
+
+// データを追加する関数のプロトタイプ宣言
+void create(CELL* startCell, int value);
+
+// 一覧を表示する関数のプロトタイプ宣言
+void index(CELL* startCell);
+
 int main() {
+	int val;
+	CELL head;
+	head.next = nullptr;
+
+	while (true) {
+		scanf_s("%d", &val);
+
+		create(&head, val);
+
+		index(&head);
+	}
 
 	return 0;
+}
+
+void create(CELL* startCell, int value) {
+	CELL* newCell;
+
+	newCell = (CELL*)malloc(sizeof(CELL));
+
+	newCell->val = value;
+	newCell->next = nullptr;
+
+	while (startCell->next != nullptr) {
+		startCell = startCell->next;
+	}
+
+	startCell->next = newCell;
+}
+
+void index(CELL* startCell) {
+	while (startCell->next != nullptr) {
+		startCell = startCell->next;
+		printf("%d\n", startCell->val);
+	}
 }
